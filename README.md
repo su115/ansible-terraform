bde4f426c4686554d68d779a8a35d944329bdcac
clone8 <ihor.prots.lviv@gmail.com>
2021-06-08 18:48:17 -0400

    fix bug 002

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # Prerequirement:
 You need ssh key pair, AWS CLI, terraform, /bin/bash
+
 #
-#Instalation:
+# Instalation:
 ```
 git clone https://github.com/su115/ansible-terraform.git
+```
+You need change provider options in main.tf
+and key option in aws_instance
+```
 terraform init
 terraform apply
-./init.sh
+./init.sh path_to_instanse_private_ssh_key
 ```
 Thats all:)
 ![resault](./photo.png)
--- init.sh
+++ init.sh
@@ -15,12 +15,17 @@ server_1=$(echo $all_ips |cut -d "\"" -f 4)
 # change ip_address on server_1 ip
 sed -i "s/ip_address/$server_1/g" ./inventory.yml
 
+# change key
+path_to_key=$1
+sed -i "s/ansibleKey/$path_to_key/g" ./* #./inventory.yml ./load.sh ./prepairs.sh
+
+
 # load data on server
 ./load.sh $server_0 "ubuntu"
 
 ### ssh to server
 # install ansible add ssh-key and start ansible
-ssh -i ./ansibleKey ubuntu@$server_0 ./prepairs.sh
+ssh -i ansibleKey ubuntu@$server_0 ./prepairs.sh
 exit
 
 echo "\n\n\nhttp://$server_1"
